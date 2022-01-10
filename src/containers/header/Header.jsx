ebcec886f539441507209a2cd8848bfca85614e4
Fabien Chareun <cacrfabfr@gmail.com>
2022-01-10 11:00:01 +0100

    fix header img

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -20,9 +20,9 @@ const Header = () => {
           <img src={people} alt="people" />
           <p>1,621 people requested access a visit in last 24 hours</p>
         </div>
-        <div className="gpt3__header-image">
-          <img src={ai} alt="ai" />
-        </div>
+      </div>
+      <div className="gpt3__header-image">
+        <img src={ai} alt="ai" />
       </div>
     </div>
   )
